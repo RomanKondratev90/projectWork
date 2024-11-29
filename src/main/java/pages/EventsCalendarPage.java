62dec85935b4fa16d7fd07c5e0e63e12d8a79b46
Roman Kondratev <kondratev@auction-house.ru>
2024-11-29 08:05:45 +0300

    Проектная работа

--- src/main/java/pages/EventsCalendarPage.java
+++ src/main/java/pages/EventsCalendarPage.java
@@ -8,9 +8,11 @@ import utils.ElementActions;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
@@ -42,9 +44,31 @@ public class EventsCalendarPage {
 
 
     public EventsCalendarPage checkActualDates() {
-        ElementsCollection dateElements = $$("span.dod_new-event__date-text").shouldHave(CollectionCondition.sizeGreaterThan(0));
-        for (SelenideElement dateElement : dateElements) {
-            System.out.println(dateElement.getText());
+        ElementsCollection eventElements = $$("div.dod_new-event__time").shouldHave(CollectionCondition.sizeGreaterThan(0));
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.forLanguageTag("ru"));
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        int currentYear = now.getYear();
+        for (SelenideElement eventElement : eventElements) {
+            try {
+                ElementsCollection dateAndTimeItems = eventElement.$$("span.dod_new-event__date-text");
+                if (dateAndTimeItems.size() >= 2) {
+                    String dateText = dateAndTimeItems.get(0).getText().trim();
+                    String timeText = dateAndTimeItems.get(1).getText().trim();
+                    String dateWithYear = dateText + " " + currentYear;
+                    LocalDate date = LocalDate.parse(dateWithYear, dateFormatter);
+                    LocalTime time = LocalTime.parse(timeText, timeFormatter);
+                    LocalDateTime eventDateTime = LocalDateTime.of(date, time);
+                    if (!eventDateTime.isBefore(now)) {
+                        System.out.println("Актуальная и будущие даты: " + eventDateTime);
+                    } else {
+                        System.out.println("Неактуальная дата и время: " + eventDateTime);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Ошибка: " + eventElement.getText());
+                e.printStackTrace();
+            }
         }
         return this;
     }
--- src/test/java/tests/checkEventsCalendarTest.java
+++ src/test/java/tests/checkEventsCalendarTest.java
@@ -22,7 +22,7 @@ public class checkEventsCalendarTest extends BaseSettings {
                 .clickBropdownTraining()
                 .clickEventsCalendar();
         new EventsCalendarPage()
-                //   .scrollToEndOfPage()
+                .scrollToEndOfPage()
                 .checkActualDates();
     }
 }
